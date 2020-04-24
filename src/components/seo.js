8f2ad1ebbd17ebdb3e17cc3d7ae5fa4956fab5fa
chris-jburke <cjburke04@gmail.com>
2020-04-24 15:57:46 -0700

    more attempts

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title, image }) {
+function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -50,6 +50,10 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           property: `image`,
+          content: `${site.siteMetadata.url}${site.siteMetadata.image}`,
+        },
+        {
+          property: `og:image`,
           content: `${site.siteMetadata.url}${site.siteMetadata.image}`,
         },
         {

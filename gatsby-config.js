2e3091c26b3cd2eb3c21df9f1538e8b372dcc8ff
chris-jburke <cjburke04@gmail.com>
2020-04-24 15:36:00 -0700

    please work

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `This site showcases the projects worked on by me, Christopher Burke during my time at General Assembly`,
     author: `cjburke04@gmail.com`,
     url: `https://christopherburke.netlify.app`,
-    image: `/images/portfolio-preview-best.jpg`,
+    image: `/static/images/portfolio-preview-best.jpg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

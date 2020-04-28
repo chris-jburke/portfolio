c6e82835808b3f15707df3989c4861e184585414
chris-jburke <cjburke04@gmail.com>
2020-04-27 21:34:57 -0700

    small spacing change

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -52,8 +52,8 @@ const ProjectsPage = () => (
     					<Box >
     						<Typography style={{"marginBottom":"10px"}}variant="h3"><span style={{"color":"white"}}>Elm Prints</span></Typography>
     						<Typography variant="body1" style={{"color":"white"}}>MERN stack E-commerce application where users can buy posters/prints of movie posters and artwork. This is a decoupled app built using Node.js with Express, Mongodb and Mongoose, React with Typescript and Material-UI. Deployed on Heroku. </Typography>
-    						<Typography variant="body1"><span style={{"color":"white"}}>Link to Elm Prints: </span><WhiteButton variant="outlined" size="small" href="http://elmprints.herokuapp.com/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
-                            <Typography variant="body1"><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/elmprints" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
+    						<Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to Elm Prints: </span><WhiteButton variant="outlined" size="small" href="http://elmprints.herokuapp.com/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
+                            <Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/elmprints" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
     					</Box>
     				</Grid>
     			</Grid>
@@ -72,8 +72,8 @@ const ProjectsPage = () => (
     					<Box>
     						<Typography variant="h3" style={{"marginBottom":"10px"}}><span style={{"color":"white"}}>Martian Terraforming</span></Typography>
     						<Typography variant="body1"><span style={{"color":"white"}}>A science fiction themed idle clicker game where players can develop a Martian colony. This app is built using a Go backend which uses Gin as a routing framework and Gorm as an ORM. The front end is built using React (with Hooks). Deployed on Heroku.</span></Typography>
-                            <Typography variant="body1"><span style={{"color":"white"}}>Link to Martian Terraforming: </span><WhiteButton variant="outlined" size="small" href="http://mars-colony-game.herokuapp.com/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
-    						<Typography variant="body1"><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/mars-colony-game" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
+                            <Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to Martian Terraforming: </span><WhiteButton variant="outlined" size="small" href="http://mars-colony-game.herokuapp.com/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
+    						<Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/mars-colony-game" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
     					</Box>
     				</Grid>
     			</Grid>
@@ -91,8 +91,8 @@ const ProjectsPage = () => (
     					<Box>
     						<Typography variant="h3" style={{"marginBottom":"10px"}}><span style={{"color":"white"}}>Lord of Stars</span></Typography>
     						<Typography variant="body1"><span style={{"color":"white"}}>An MVC framework chat application that only allow users to chat using moving quotes from the Lord of the Rings. Built using Node.js with Express, Sequelize, EJS, web sockets, and the Lord of the Rings API. Deployed on Heroku.</span></Typography>
-    						<Typography variant="body1"><span style={{"color":"white"}}>Link to Lord of Stars: </span><WhiteButton variant="outlined" size="small" href="http://lord-of-stars.herokuapp.com/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
-                            <Typography variant="body1"><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/project2-lord-of-stars" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>    					
+    						<Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to Lord of Stars: </span><WhiteButton variant="outlined" size="small" href="http://lord-of-stars.herokuapp.com/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
+                            <Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/project2-lord-of-stars" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>    					
                         </Box>
     				</Grid>
     			</Grid>
@@ -110,8 +110,8 @@ const ProjectsPage = () => (
                         <Box>
                             <Typography variant="h3" style={{"marginBottom":"10px"}}><span style={{"color":"white"}}>Checkers</span></Typography>
                             <Typography variant="body1"><span style={{"color":"white"}}>This is a fully front-end web application where two players can play a game of American Checkers. Built using vanilla Javascript, CSS, and HTML. Deployed on Github.</span></Typography>                          
-                            <Typography variant="body1"><span style={{"color":"white"}}>Link to Checkers: </span><WhiteButton variant="outlined" size="small" href="https://chris-jburke.github.io/project1-checkers/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
-                            <Typography variant="body1"><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/project1-checkers" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>                            
+                            <Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to Checkers: </span><WhiteButton variant="outlined" size="small" href="https://chris-jburke.github.io/project1-checkers/" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>
+                            <Typography variant="body1" style={{"marginTop":"5px"}}><span style={{"color":"white"}}>Link to the Github: </span><WhiteButton variant="outlined" size="small" href="https://github.com/chris-jburke/project1-checkers" target="_blank" rel="noopener norefferrer">here</WhiteButton></Typography>                            
                         </Box>
                     </Grid>
                 </Grid>
